c80ec77862f91bbe6f508ab8659f7d0b3e729b20
adowlin <dowlingalison@gmail.com>
2021-06-04 19:52:16 +0000

    Add login form & wire up functionality to login.html template

--- app.py
+++ app.py
@@ -33,11 +33,12 @@ def search():
     return render_template("recipes.html", recipes=recipes)
 
 
+# Authentication functionality for Register, Login, Profile adapted from
+#  Code Institute Flask Mini Project coursework:
+# https://github.com/adowlin/flask-task-manager/blob/master/app.py
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    # Authentication functionality adapted from Code Institute
-    # Flask Mini Project coursework:
-    # https://github.com/adowlin/flask-task-manager/blob/master/app.py
     if request.method == "POST":
         # check if username already exists in db
         existing_user = mongo.db.users.find_one(
@@ -58,6 +59,35 @@ def register():
         flash("Registration Successful!")
         return redirect(url_for("register", username=session["user"]))
     return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(
+                    request.form.get("username")))
+                return redirect(url_for(
+                    "login", username=session["user"]))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
