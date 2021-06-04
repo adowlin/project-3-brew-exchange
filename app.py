b146cb272fb0bd99879e4226a6fed36e89ad12d6
adowlin <dowlingalison@gmail.com>
2021-06-04 00:09:54 +0000

    Add search functionality to app.py & wire up to recipes.html search form

--- app.py
+++ app.py
@@ -25,6 +25,13 @@ def get_recipes():
     return render_template("recipes.html", recipes=recipes)
 
 
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    query = request.form.get("query")
+    recipes = list(mongo.db.recipes.find({"$text": {"$search": query}}))
+    return render_template("recipes.html", recipes=recipes)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
