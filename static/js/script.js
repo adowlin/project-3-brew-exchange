074275fdca4ec8316e740a555ce6af13602bfbaa
adowlin <dowlingalison@gmail.com>
2021-06-08 17:58:54 +0000

    Add JS functionality to add padding-top to page content when navbar is sticky

--- static/js/script.js
+++ static/js/script.js
@@ -10,18 +10,22 @@ $(document).ready(function () {
         myFunction()
     };
 
-    // Get the navbar
+    // Get the navbar & H2 element below it
     var navbar = document.getElementById("nav_bar");
+    var searchHeader = document.getElementById("search-header");
 
     // Get the offset position of the navbar
     var sticky = navbar.offsetTop;
 
-    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    // Add the "sticky" class to the navbar & "content" class to the header when you reach nav scroll position.
+    // Remove "sticky" & "content" when you leave the nav scroll position
     function myFunction() {
         if (window.pageYOffset >= sticky) {
             navbar.classList.add("sticky")
+            searchHeader.classList.add("content");
         } else {
             navbar.classList.remove("sticky");
+            searchHeader.classList.remove("content");
         }
     }
 });
